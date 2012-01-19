e9be0623a7bb67b5a06edfaee9ab5714394f7669
sander1 <sander@plexapp.com>
2012-01-20 00:21:13 +0100

    Add *some* cacheTime; cache thumbs; add PlexPluginRegions key

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,32 +1,28 @@
-# PMS plugin framework
 import datetime
 
-####################################################################################################
+NAME = L('Title')
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
-VIDEO_PREFIX = "/video/espn3"
-
-NAME          = L('Title')
-ART           = 'art-default.jpg'
-ICON          = 'icon-default.png'
-
-ESPN3_REPLAY  = "http://espn.go.com/espn3/feeds/replay"
-ESPN3_LIVE    = "http://espn.go.com/espn3/feeds/live"
-ESPN_PLAYER   = "http://espn.go.com/watchespn/player/_/source/espn3/id/"
+ESPN3_REPLAY = "http://espn.go.com/espn3/feeds/replay"
+ESPN3_LIVE = "http://espn.go.com/espn3/feeds/live"
+ESPN_PLAYER = "http://espn.go.com/watchespn/player/_/source/espn3/id/"
 
 ####################################################################################################
-
 def Start():
 
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, L('VideoTitle'), ICON, ART)
+	Plugin.AddPrefixHandler("/video/espn3", VideoMainMenu, L('VideoTitle'), ICON, ART)
 
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
+	MediaContainer.art = R(ART)
+	MediaContainer.title1 = NAME
+	DirectoryItem.thumb = R(ICON)
 
+####################################################################################################
 def VideoMainMenu():
+
 	dir = MediaContainer(viewGroup="List")
 	dir.Append(Function(DirectoryItem(VideoPage, "Live Games"), pageUrl = ESPN3_LIVE, predict="/event/event"))
 	dir.Append(Function(DirectoryItem(VideoPage, "All Replay Games"), pageUrl = ESPN3_REPLAY, predict="/event/event"))
@@ -39,35 +35,54 @@ def VideoMainMenu():
 	dir.Append(Function(DirectoryItem(VideoPage, "Softball"), pageUrl = ESPN3_REPLAY, predict="/event/event/sport[@code='SB']/.."))
 	dir.Append(Function(DirectoryItem(VideoPage, "Cricket"), pageUrl = ESPN3_REPLAY, predict="/event/event/sport[@code='CR']/.."))
 	dir.Append(Function(DirectoryItem(VideoPage, "Rugby"), pageUrl = ESPN3_REPLAY, predict="/event/event/sport[@code='RG']/.."))
+
 	return dir
-	
+
 ####################################################################################################
 def VideoPage(sender, pageUrl, predict):
-    if "live" in pageUrl:
-        dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", noCache=True)
-    else:
-        dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", noCache=True)
-    content = XML.ElementFromURL(pageUrl, cacheTime=0).xpath(predict)
-    eventMap = dict()
-    for item in content:
-        eventId = item.get('id')
-        link = ESPN_PLAYER + eventId
-        image = item.xpath('./thumbnail/large')[0].text
-        title = item.xpath('./league')[0].text + ' - ' + item.xpath('./name')[0].text
-        startTime = int(item.xpath('./startTimeGmtMs')[0].text)/1000
-        subtitle = 'Originally Aired: ' + datetime.datetime.fromtimestamp(startTime).strftime('%a %b %d, %Y')
-        
-        eventList = eventMap.get(startTime)
-        if eventList == None:
-		    eventList = []
-		    eventMap[startTime] = eventList
+
+	if "live" in pageUrl:
+		dir = MediaContainer(title2=sender.itemTitle, viewGroup="List", noCache=True)
+	else:
+		dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", noCache=True)
+
+	content = XML.ElementFromURL(pageUrl, cacheTime=300).xpath(predict)
+	eventMap = dict()
+
+	for item in content:
+		eventId = item.get('id')
+		link = ESPN_PLAYER + eventId
+		image = item.xpath('./thumbnail/large')[0].text
+		title = item.xpath('./league')[0].text + ' - ' + item.xpath('./name')[0].text
+		startTime = int(item.xpath('./startTimeGmtMs')[0].text)/1000
+		subtitle = 'Originally Aired: ' + datetime.datetime.fromtimestamp(startTime).strftime('%a %b %d, %Y')
+		eventList = eventMap.get(startTime)
+
+		if eventList is None:
+			eventList = []
+			eventMap[startTime] = eventList
+
 		# Tuple order here matters
-        eventList.append((link, image, title, subtitle))
-    events = eventMap.keys()[:]
-    events.sort()
-    events.reverse()
-    for eventTime in events:
+		eventList.append((link, image, title, subtitle))
+
+	events = eventMap.keys()[:]
+	events.sort()
+	events.reverse()
+
+	for eventTime in events:
 		for event in eventMap[eventTime]:
 			# Same order they went into the tuple
-			dir.Append(WebVideoItem(url=event[0], subtitle=event[3], title=event[2], thumb=event[1]))
-    return dir
+			dir.Append(WebVideoItem(url=event[0], subtitle=event[3], title=event[2], thumb=Function(GetThumb, url=event[1])))
+
+	return dir
+
+####################################################################################################
+def GetThumb(url):
+
+	try:
+		data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
+		return DataObject(data, 'image/jpeg')
+	except:
+		pass
+
+	return Redirect(R(ICON))
